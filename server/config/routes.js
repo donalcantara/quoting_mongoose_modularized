e07752454440a513fd95a633658204c5bfee5470
Don Alcantara <don.alcantara@yahoo.com>
2016-07-13 12:33:28 -0700

    first commit

---
+++ server/config/routes.js
@@ -0,0 +1,15 @@
+var quotes = require('../controllers/quotes.js');
+module.exports = function(app) {
+  app.get('/', function(req, res) {
+    res.render('index')
+  })
+  app.post('/quotes', function(req,res){
+    quotes.create(req, res)
+  })
+  app.get('/main', function (req, res) {
+    quotes.show(req, res)
+  })
+  app.get('/delete/:id', function(req,res){
+    quotes.delete(req, res)
+  })
+}
---
+++ server/controllers/quotes.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+var Quote = mongoose.model('Quote');
+
+module.exports = {
+  show: function(req, res) {
+    Quote.find({}, function(err, quotes) {
+      res.render('main', {quotes: quotes});
+    })
+  },
+  create: function(req, res) {
+    var quote = new Quote({name: req.body.name, quote: req.body.quote});
+    quote.save(function(err) {
+      if(err){
+        console.log("something went wrong");
+      } else {
+        res.redirect('/main');
+      }
+    })
+  },
+  delete: function(req, res){
+  Quote.remove({_id: req.params.id}, function(err, whatever){
+    Quote.find({}, function(err, quotes) {
+      res.render('main', {quotes: quotes});
+    })
+  })
+}
+}
